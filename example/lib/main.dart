555e0b051f8700aeadc3e3a9cafedb68c575835c
Rael San Juan <62531088+rynssnjn@users.noreply.github.com>
2022-10-02 15:26:13 +0800

    Update main.dart

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:example/contact.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(const MyApp())
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
